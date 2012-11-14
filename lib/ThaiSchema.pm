acddd8cd3e2221b0a7f5c70afc8cd1bf02d3641c
tokuhirom <tokuhirom@gmail.com>
2012-11-14 09:30:09 +0900

    initial import

---
+++ lib/ThaiSchema.pm
@@ -0,0 +1,39 @@
+package ThaiSchema;
+use strict;
+use warnings;
+use 5.010001;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+ThaiSchema - ...
+
+=head1 SYNOPSIS
+
+  use ThaiSchema;
+
+=head1 DESCRIPTION
+
+ThaiSchema is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF@ GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
